610f48dd1c42395ba573eebd6a19959548518730
ranjeetkb <84655042+ranjeetkb@users.noreply.github.com>
2022-02-25 11:33:29 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ console.log("Demo Node Example");
 console.log("Hello World")
 var dateTime = new Date();
 console.log(dateTime);
+console.log(dateTime.getDate()+"-"+(dateTime.getMonth()+1)+"-"+dateTime.getFullYear());
